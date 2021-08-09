0985d4012f20ea12837040b45eeefa88c49fb40d
Chris <57379467+pomgolian@users.noreply.github.com>
2021-08-09 13:30:39 +1000

    Create servo_calibrate.py

---
+++ servo_calibrate.py
@@ -0,0 +1,50 @@
+import pi_servo_hat
+
+global servo_number
+global direction
+servo_number = 0
+direction = 1
+servo_value = 100
+
+servo = pi_servo_hat.PiServoHat()
+
+# Restart Servo Hat (in case Hat is frozen/locked)
+servo.restart()
+
+
+print('Enter 1, 2 or 3 for sun, mask or moon')
+print('Enter f for forward or r for reverse for servo direct')
+print('Enter exit to exit')
+while 1:
+    key_press = input('Press a key ')
+
+    if key_press == 'exit':
+        exit()
+
+    if key_press == '1':
+        servo_number = 0
+        servo_value = 100
+        print('Servo sun has been selected, servo_value set to 100')
+
+    if key_press == '2':
+        servo_number = 1
+        servo_value = 100
+        print('Servo mask has been selected, servo_value set to 100')
+
+    if key_press == '3':
+        servo_number = 2
+        servo_value = 100
+        print('Servo moon has been selected, servo_value set to 100')
+
+    if key_press == 'f':
+        print('Direction set to forward')
+        direction = 1
+
+    if key_press == 'r':
+        print('Direction set to reverse')
+        direction = -1
+
+    if len(key_press) == 0:
+        servo_value += direction
+        print('move servo {} in the direction {} with value {}'.format(servo_number, direction, servo_value))
+        servo.move_servo_position(servo_number, servo_value, 180)
